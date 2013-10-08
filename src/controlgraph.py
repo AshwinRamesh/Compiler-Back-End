001f9c98198682b4a5845c60e79af91835e9d1a5
Alexander Hogue <defaultnamehere@gmail.com>
2013-10-08 12:53:19 +1100

    Rewrote everything (almost) to be neater and work correctly(!)

--- src/controlgraph.py
+++ src/controlgraph.py
@@ -1,132 +1,100 @@
-# @author Ashwin Ramesh
-## This class defines both the node and the graph of the control flow graph ##
-
-class ControlNode:
+class Node:
 
     def __init__(self, block_id, instructions):
-        # TODO check if instructions is array
-        self.id = int(block_id)
+        self.node_id = block_id
         self.instructions = instructions
-        self.pre_nodes = {}
-        self.suc_nodes = {}
-
-    def set_instructions(self, instructions):
-        # TODO check if instuctions is array
-        self.instructions = instructions
-        return True
+        self.in_nodes = []
+        self.out_nodes = []
 
     def add_instruction(self, instruction):
         self.instructions.append(instruction)
 
     def set_id(self, block_id):
-        self.id = block_id
+        self.node_id = block_id
 
-    def set_predecessor(self, pre_node):
-        if pre_node == None:
-            self.pre_nodes = None
-            return True
-        if pre_node.id not in self.pre_nodes:
-            self.pre_nodes[pre_node.id] = pre_node
-            return True
-        return False
+    def get_id(self):
+        return self.node_id
 
-    def set_successor(self, suc_node):
-        if suc_node == None:
-            self.suc_nodes = None
-            return True
-        if suc_node.id not in self.suc_nodes:
-            self.suc_nodes[suc_node.id] = suc_node
-            return True
-        return False
+    def add_in_node(self, node):
+        self.in_nodes.append(node)
+
+    def add_out_node(self, node):
+        self.out_nodes.append(node)
 
     def get_instructions(self):
         return self.instructions
 
-    def get_instruction_by_key(self, key): # TODO
-        pass
+    def get_in_nodes(self):
+        return self.in_nodes
 
-    def get_predecessors(self):
-        return self.pre_nodes
+    def get_out_nodes(self):
+        return self.out_nodes
 
-    def get_successors(self):
-        return self.suc_nodes
+    def __str__(self):
+        return "Node ID: %s \n Instructions: %s \n Predecessors: %s \n Successors: %s \n" % (self.node_id, self.instructions, self.in_nodes, self.out_nodes)
+    def __repr__(self):
+        return str(self)
 
-    def to_str(self):
-        if type(self.pre_nodes) is dict or type(self.pre_nodes) is list:
-            pre_str = str(self.pre_nodes.keys())
-        else:
-            pre_str = "None"
-        if type(self.suc_nodes) is dict or type(self.suc_nodes) is list:
-            suc_str = str(self.suc_nodes.keys())
-        else:
-            suc_str = "None"
-        return "Node ID: %d \n Instructions: %s \n Predecessors: %s \n Successors: %s \n" %(self.id, str(self.instructions), pre_str, suc_str)
+class CFG:
 
-class ControlGraph:
+    def __init__(self, program):
+        #TODO: Are we the worst?
+        self.start = Node("START", [])
+        self.end = Node("END", [])
+        self.nodes = [self.start, self.end]
+        self.current_block_number = 0
 
-    def __init__(self, function_name):
-        self.name = function_name
-        self.nodes = {}
+        current_instructions = []
+        for name, function in program.iteritems():
+            blocks = function['blocks'] #I'm so sorry Kenni
 
-    def set_head(self, head_node):
-        self.head = head_node
-        head_node.set_predecessor(None)
+            #for every instruction in the block
+            #if we hit a br or ret
+            #then the code above this (but before any other br or rets) is a new node
+            #keep going
+            for block_id, instructions in blocks.iteritems():
 
-    def get_head(self):
-        return self.head
+                for instruction in instructions:
+                    current_instructions.append(instruction)
+
+                    if instruction[0] in ("br", "ret"):
+                        #make a node with the current instructions, and clear the list for any later nodes
+                        self.add_new_node(current_instructions)
+                        current_instructions = []
+                #make a node at the end of the instructions
+                if current_instructions:
+                    self.add_new_node(current_instructions)
+                    current_instructions = []
+
+
+
+        print self.nodes
+
+
+    def add_new_node(self, current_instructions):
+        node = Node(self.current_block_number, current_instructions)
+        self.add_node(node)
+        self.current_block_number += 1
+
+    def get_start(self):
+        return self.start
+
+    def get_end(self):
+        return self.end
 
     def add_node(self, node):
-        if not self.check_node_exists(node.id):
-            self.nodes[node.id] = node # place in array by block id key
-            return True
-        return False
+        self.nodes.append(node)
 
     def get_nodes(self):
         return self.nodes
 
-    def get_node(self, id):
-        if self.check_node_exists(id):
-            return self.nodes[id]
-        return False
+    def get_node(self, node_id):
+        #TODO: map id -> node if we do this a lot
+        #linearsearch
+        for node in self.nodes:
+            if node.get_id() == node_id:
+                return node
 
     def create_intermediate_code(self): # TODO
         pass
 
-    def check_node_exists(self, id):
-        if id in self.nodes:
-            return True
-        return False
-
-    @staticmethod
-    def create_cfg(name, function):
-        blocks = function['blocks']
-        if len(blocks) < 0:
-            return False
-        g = ControlGraph(name)
-        # Add the nodes
-        for block_id, block in blocks.items():
-            node = ControlNode(block_id, block)
-            g.add_node(node)
-        # Make nodes directed and joint
-        for block_id, block in blocks.items():
-            node = g.nodes[block_id]
-            if block_id == 0:
-                g.set_head(node) # Set head node
-            last_instruction = block[-1]
-            if last_instruction[0] == "br": # last instruction is branching
-                node_true = g.nodes[int(last_instruction[2])] # branching node for true
-                node_false = g.nodes[int(last_instruction[3])] # branching node for false
-                # Set pre/succ
-                node.set_successor(node_true)
-                node.set_successor(node_false)
-                node_true.set_predecessor(node)
-                node_false.set_predecessor(node)
-            if last_instruction[0] == "ret": # last instruction is return
-                node.set_successor(None)
-        g.print_graph()
-        return g
-
-    def print_graph(self):
-        print "Graph of function: %s \n" %(self.name)
-        for key, node in self.nodes.items():
-            print node.to_str()
--- src/main.py
+++ src/main.py
@@ -1,30 +1,15 @@
 import parser
-from optimisation_exceptions import *
-from controlgraph import ControlNode, ControlGraph
+from controlgraph import CFG 
 import sys
 
-functions = {}
 
 def main():
-    global functions
-    try:
-        if len(sys.argv) <= 1:
-            raise FileNotGivenException()
-        graphs = {}
-        functions = parser.process_file(sys.argv[1])
-        print functions
-        for key, function in functions.items():
-            graphs[key] = ControlGraph.create_cfg(key, function)
-    except IOError:
-        print "Error: File does not exist. Cannot optimise."
-        exit(1)
-    except FileNotGivenException as e:
-        print "Error: No input file given. Cannot optimise."
-        exit(1)
-    #except IndexError:
-    #    print "Error: Corrupted intermediate file. Cannot optimise."
-    #    exit(1)
+    program = parser.process_file(sys.argv[1])
+    print program
+    #for key, function in functions.items():
+    #    graphs[key] = ControlGraph.create_cfg(key, function)
 
+    cfg = CFG(program)
 
 if __name__ == "__main__":
     main()
